354c8a5a02d20513b9ece9473ccd70c296a216f2
Jovica <jovicadufour@gmail.com>
2025-10-16 00:44:02 +0200

    [PROGRESS] strcmp

---
+++ ft_strcmp.s
@@ -0,0 +1,25 @@
+bits 64
+
+section .text
+global _ft_strcmp
+
+; int ft_strcmp(const char *s1, const char *s2)
+_ft_strcmp:
+	xor rcx, rcx
+
+.loop:
+	mov al, [rsi + rcx]	; copy s2[i] into volatile 8bits register al
+	mov dl, [rdi + rcx]	; copy s1[i] into volatile 8bits register dl
+	cmp dl, al			; compare the 2 char
+	jne .return			; if different, return 
+
+	test dl, dl			; performs s1[i] & s1[i]. Sets the processors Zero Flag at 1 is '\0'
+	je .return			; return if '\0'
+
+	inc rcx				; increments the counter
+	jmp .loop			; restarts the loop
+
+.return:
+	sub dl, al			; substracts s2[i] from s1[i]
+	movsx rax, dl		; expands the result to 64bits and put it into rax
+	ret					; return to the caller
--- main.c
+++ main.c
@@ -4,11 +4,12 @@
 
 size_t	ft_strlen(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
 
 int	main(int argc, char **argv)
 {
-	if (argc != 3)
-		return (printf("Wrong number of argument. Expected : two"), 1);
+	if (argc < 3)
+		return (printf("Wrong number of argument. Expected : two (COMMAND, arg)\n"), 1);
 	if (!strcmp(argv[1], "LEN"))
 	{
 		printf("Calculated length of %s is %zu\n", argv[2], ft_strlen(argv[2]));
@@ -42,7 +43,14 @@ int	main(int argc, char **argv)
 
 		printf("Copy with dest too small with std function... Dest is %s\n", strcpy(PROOF_dest2, src));	
 		printf("Copy with dest too small of src into dest... Dest is %s\n", ft_strcpy(dest2, src));
+	}
+	else if (!strcmp(argv[1], "COMPARE"))
+	{
+		if (argc != 4)
+			return (printf("Wrong number of argument. Expected : 3 (COMMAND, arg1, arg2)\n"), 1);
 
+		printf("Compare on two args gives %d\n", ft_strcmp(argv[2], argv[3]));
+		printf("STD compare on two args gives %d\n", strcmp(argv[2], argv[3]));
 	}
 
 	return (0);
